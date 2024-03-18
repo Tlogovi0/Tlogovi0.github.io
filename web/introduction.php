af0d7991204b1ec93a903c88f220a27caac3c7b6
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-18 19:02:08 -0400

    Update introduction.php

--- web/introduction.php
+++ web/introduction.php
@@ -1,1 +1,28 @@
+<figcaption> It is me  </figcaption>
 
+<ul>
+<li><b> Background:</b> I am of Togolese nationality. I grew up in Togo. I had my high school diploma and after that I had my diploma in marketing management. My first language is French.
+</li>
+<li><b>Professional Background: </b>I had work at the airport and Amazon. I drive Uber and Lyft too.
+</li>	
+<li><b>Academic Background: </b>I have got some certifications and badges. I will get my Associate Degree this semester in Database Development/Business Intelligence.
+</li>	
+<li><b>Background in this Subject:</b> I worked more in the field of advertising products on websites. I hope that with this course I could expand and deepen my knowledge of the web.
+</li>
+<li><b>Primary Computer Platform:</b> Windows 10 Home and office.
+</li>	
+<li><b>Courses I'm Taking & Why:</b>
+<dt><b>WEB 250 :</b> Database Driven; Websites substitute course for CTI</dt>
+<dt><b>CSC124 : </b>Introduction to Data Science Programming </dt>
+<dt><b>CSC152: </b>SAS</dt>
+<dt><b>DBA285 : </b>Database Warehousing and Mining </dt>
+</li>
+</ul>
+<p>I am taking this course because it is a requirement for my program and i would like to get more knowledges on the Web.
+</p>
+<ul>	
+<li><b>Funny/Interesting item to Remember me by:</b> Many people who know me say that i am jovial , welcoming and joyful.
+</li>	
+<li><b>I'd also like to Share: </b>I like to spend my time doing sporting and relaxing activities.
+</li>
+</ul>
