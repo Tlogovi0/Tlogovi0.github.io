ea51819e6ea046b34ff5c3d209758e5f8dfcac08
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-21 15:00:05 -0400

    Update contract.php

--- contents/contract.php
+++ contents/contract.php
@@ -1,1 +1,9 @@
+<p>
+I, Tete. Logovi agrees to abide by the terms of the course contract in my SEMESTER 2024, Web 250 with my instructor, Professor D.I. von Briesen. 
+I understand that all work that I do on publicly available online tools will be available to the world, and will not put information there that is inappropriate for schoolwork, or that I wish to keep private.
 
+I swear that I will only use these resources to learn and will not cut and paste code except where I have properly given credit (i.e. external libraries) and never from my classmates.
+
+</p>
+<p>
+Signed: <i>Tete K F. Logovi , 28/01/2024 </i>
