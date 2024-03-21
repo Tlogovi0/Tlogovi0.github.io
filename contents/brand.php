2e5029521ccbc145c4c1bbf2cb101b24786e8fc2
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-21 15:01:30 -0400

    Update brand.php

--- contents/brand.php
+++ contents/brand.php
@@ -1,1 +1,2 @@
-
+<h2>Concept/Idea/Objective/Mission</h2>
+<h5>"The concept is to build and help people find the homes of their dreams at a lower cost and ensure that their homes provide you with peace and a heavenly environment."</h5>
