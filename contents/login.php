c8b27db4e621f91f62c535e28abe7caa71fb3616
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-24 11:30:35 -0400

    Update login.php

--- contents/login.php
+++ contents/login.php
@@ -101,5 +101,28 @@
             }
         }
     </script>
+	<footer> 
+	
+		<nav class="navbar">
+ 
+                <a href="https://github.com/Tlogovi0/Tlogovi0.github.io">Github.io</a>   |
+		<a href="https://www.freecodecamp.org/fccc35594b7-aa93-4d23-a1da-68675e421126">freeCodeCamp</a>   |
+		<a href=https://www.codecademy.com/profiles/Tlogovi0">Codecademy</a>   |
+		<a href="https://pathfinder.w3schools.com/profile">W3schools</a>   |
+		<a href="https://www.linkedin.com/learning/?u=76141674">LinkedIn</a>   |      
+</footer>
+
+<p>&copy; 2024 Home Paradise. All rights reserved.</p>   
+		<p>
+		<a href="http://validator.w3.org/check?uri=referer" style = "text-decoration: none">
+		  <img src="images/html_validation_button.gif" alt="Validate HTML" />
+		</a>
+		 
+		<a href="http://jigsaw.w3.org/css-validator/validator?uri=https://tlogovi0.github.io/web250.io/index.html">
+		 <img src="images/validation_button_css_rectangle_blue.gif" alt="Validate CSS" >
+
+		</a>
+		</p>
+	
 </body>
 </html>
