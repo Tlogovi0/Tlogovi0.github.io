4d355488528f080fccf577a89ffc4b315c54ae6b
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-26 19:30:52 -0400

    Update index.php

--- contents/index.php
+++ contents/index.php
@@ -2,5 +2,5 @@
 <h1><h1> Home</h1></h1>
 <h2>Welcome to my Web page.</h2>
 <p>You can find all the necessary information about me in my introduction page:</p>
-<a href="introduction.html">Introduction</a>
+<a href="index.php?page=introduction">introduction</a>
 		
