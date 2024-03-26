e287c723ebbfbfd54d4bd2cba19549ac8064a29b
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-26 13:02:45 -0400

    Update contract.php

--- contents/contract.php
+++ contents/contract.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link href="./css/default.css" rel="stylesheet" type="text/css">	
-             <title>Tete Logovi the real Eagle | WEB250| Home</title>
-</head>
 
-		<h1>Tete Logovi the real Eagle| WEB250</h1
-<header>
-<nav class="navbar">
-
-	<a href="index.htm">Home</a>   |
-	<a href="introduction.html">Introduction</a>   |
-	<a class="active" href="contract.html">Contract</a>   |
-	<a href="brand.html">Brand</a>   |
-	<a href="form.html">Form</a>   |
-</nav>
-</header>
-</header>
 	<h1><h1>Contract</h1></h1>
-	<body>
-	<div class="fab">
-<p id="center">
+	
 <p>
 I, Tete. Logovi agrees to abide by the terms of the course contract in my SEMESTER 2024, Web 250 with my instructor, Professor D.I. von Briesen. 
 I understand that all work that I do on publicly available online tools will be available to the world, and will not put information there that is inappropriate for schoolwork, or that I wish to keep private.
@@ -32,30 +11,3 @@ I swear that I will only use these resources to learn and will not cut and paste
 <p>
 Signed: <i>Tete K F. Logovi , 28/01/2024 </i>
 </p>
-	</div></p>
-	</body>
-<footer>
-<nav class="navbar">
-               <a href="https://github.com/Tlogovi0/Tlogovi0.github.io">Github.io</a>   |
-		<a href="https://www.freecodecamp.org/fccc35594b7-aa93-4d23-a1da-68675e421126/">freeCodeCamp</a>   |
-		<a href=https://www.codecademy.com/profiles/Tlogovi0">Codecademy</a>   |
-		<a href="https://pathfinder.w3schools.com/profile">W3schools</a>   |
-		<a href="https://www.linkedin.com/learning/?u=76141674">LinkedIn</a>   |
-		
-	</nav>
-
-</footer>
-		<p>&copy; 2024 Home Paradise. All rights reserved.</p>   
-		
-		<a href="http://validator.w3.org/check?uri=referer" style = "text-decoration: none">
-		  <img src="images/html_validation_button.gif" alt="Validate HTML" />
-		</a>
-		 
-		<a href="http://jigsaw.w3.org/css-validator/check/referer" style = "text-decoration: none">
-		 <img src="images/validation_button_css_rectangle_blue.gif" alt="Validate CSS" />
-		</a>
-		
-		
-	
-</body>
-</html>
