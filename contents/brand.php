b253b984b96d563384009b04e1ce6e3073b0bc76
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-26 10:50:34 -0400

    Update brand.php

--- contents/brand.php
+++ contents/brand.php
@@ -1,2 +1,95 @@
-<h2>Concept/Idea/Objective/Mission</h2>
+<!DOCTYPE html>
+<html>
+<head>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
+<link href="./css/default.css" rel="stylesheet" type="text/css">	
+<title>Tete Logovi the Real Eagle|WEB250|Brand</title>
+</head>
+	<h1>Tete Logovi the Real Eagle|WEB250</h1>
+		
+<nav class="navbar">
+
+	<a href="index.htm">Home</a>   |
+	<a href="introduction.html">Introduction</a>   | 
+	<a href="contract.html">Contract</a>   |
+	<a href="brand.html">Brand</a>   |
+	<a href="form.html">Form</a>   |
+	</nav>
+	<h1><h1> Brand</h1></h1>
+	<body>
+
+	<div class="fab">
+<p id="justify">
+		<h1>Tete Logovi's HOME PARADISE</h1>
+	<img src="./images/logo-png.png" width="1200" height="1200" alt="Profile Logovi" class="center">
+	<div class="fab">
+<p id="center">
+        <p><em>"Get your dream house with us."</em></p>
+	            <h2>Concept/Idea/Objective/Mission</h2>
 <h5>"The concept is to build and help people find the homes of their dreams at a lower cost and ensure that their homes provide you with peace and a heavenly environment."</h5>
+</nav>
+
+</header>
+
+<div class="fab">
+<p id="center">
+
+<h3>The brand colors and fonts will be as follow :</h3>
+	
+ <div class="fab">
+<p id="center">
+-background color : bleue
+<div class="fab">
+<p id="center">
+-navigation link : white
+<div class="fab">
+<p id="center">
+-brand background highlighted color: indigo
+<div class="fab">	
+<p id="center">
+-Footer link colors: white.
+<div class="fab">	
+<p id="center">
+-font-family: Lucida Console", Monaco, monospace.
+<div class="fab">	
+<p id="center">	
+-background-color: bleue;
+		
+</p></div></p></div></p></div></p></div></p></div></p></div></p></div></p></div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<footer> 
+	
+	<nav class="navbar">
+	
+		<a href="https://github.com/Tlogovi0/Tlogovi0.github.io">Github.io</a>   |
+		<a href="https://www.freecodecamp.org/fccc35594b7-aa93-4d23-a1da-68675e421126/">freeCodeCamp</a>   |
+		<a href=https://www.codecademy.com/profiles/Tlogovi0">Codecademy</a>   |
+		<a href="https://pathfinder.w3schools.com/profile/">W3schools</a>   |
+		<a href="https://www.linkedin.com/learning/?u=76141674">LinkedIn</a>   |
+
+	</nav>
+	</footer>
+	<p>&copy; 2024 Home Paradise. All rights reserved.</p>   
+		<p>
+		<a href="http://validator.w3.org/check?uri=referer" style = "text-decoration: none">
+		  <img src="images/html_validation_button.gif" alt="Validate HTML" />
+		</a>
+		 
+		<a href="http://jigsaw.w3.org/css-validator/check/referer" style = "text-decoration: none">
+		 <img src="images/validation_button_css_rectangle_blue.gif" alt="Validate CSS" />
+		</a>
+	
+		</p>
+
+</body>
+</html>
+	 
