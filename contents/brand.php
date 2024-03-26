2c2a9f4a02f5f2e10f29edfc5147ed36c6689c4c
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-26 13:28:48 -0400

    Update brand.php

--- contents/brand.php
+++ contents/brand.php
@@ -3,13 +3,9 @@
 	
 		<h1>Tete Logovi's HOME PARADISE</h1>
 	<img src="./images/logo-png.png" width="1200" height="1200" alt="Profile Logovi" class="center">
-	<div class="fab">
-<p id="center">
         <p><em>"Get your dream house with us."</em></p>
 	            <h2>Concept/Idea/Objective/Mission</h2>
 <h5>"The concept is to build and help people find the homes of their dreams at a lower cost and ensure that their homes provide you with peace and a heavenly environment."</h5>
-
-
 <h3>The brand colors and fonts will be as follow :</h3>
 	
  <div class="fab">
