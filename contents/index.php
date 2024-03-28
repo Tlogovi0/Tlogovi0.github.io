6b1249b5661c4443717900a798873932685dfa0b
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-27 21:39:30 -0400

    Delete contents/index.php

--- contents/index.php
+++
@@ -1,6 +0,0 @@
-
-<h1><h1> Home</h1></h1>
-<h2>Welcome to my Web page.</h2>
-<p>You can find all the necessary information about me in my introduction page:</p>
-<a href="index.php?page=introduction">introduction</a>
-		
