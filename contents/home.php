232961e23aec23f3ee80724aeb8c54c892b62139
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-27 23:03:06 -0400

    Update home.php

--- contents/home.php
+++ contents/home.php
@@ -1,3 +1,3 @@
 <h2>Welcome to my Web page.</h2>
 		<p>You can find all the necessary information about me in my introduction page:</p>
- <a href="index.php?page=introduction">
+ <a href="index.php?page=introduction">Introduction</a>
