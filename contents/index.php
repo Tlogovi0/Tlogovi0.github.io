352b154658f4c78af60fad97a1cfe66073b79297
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-03-27 22:45:18 -0400

    Update index.php

--- contents/index.php
+++ contents/index.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href="./css/default.css" rel="stylesheet" type="text/css">	
+             <title>Tete Logovi the real Eagle | WEB250|Home</title>
+</head>
+<body>
+	<header>
+		<h1>Tete Logovi the real Eagle| WEB250</h1>
 
+<nav class="navbar">
+
+	<a href="index.php?page=home">Home</a>   |
+	<a href="index.php?page=introduction">Introduction</a>   |
+	<a href="index.php?page=contract">Contract</a>   |
+	<a href="index.php?page=brand">Brand</a>   |
+	<a href="index.php?page=form">Form</a>   |   
+	<a href="index.php?page=login">Login</a>   |   
+
+</nav>
