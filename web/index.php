c490cfabc8922331f37570255dbcc983450ed180
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-04-12 19:30:32 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -15,7 +15,8 @@
         <a href = "?p=../contents/contract.php">Contract</a>   | 
        <a href = "?p=../contents/brand.php">brand</a> |
 	<a href = "?p=../contents/fizzBuzzBang.php">FizzBuzzBang</a>| 
-	<a href = "?p=../contents/login.php">login</a>   | 
+	<a href = "?p=../contents/login.php">login</a> | 
+	<a href = "?p=../contents/findMe.php">FindMe</a>| 
 
 
 
