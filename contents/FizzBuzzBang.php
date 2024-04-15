d06339ffe9b5dbeb3d67e89b85c428bc98b4aeef
Tlogovi0 <156595548+Tlogovi0@users.noreply.github.com>
2024-04-15 19:35:21 -0400

    Update FizzBuzzBang.php

--- contents/FizzBuzzBang.php
+++ contents/FizzBuzzBang.php
@@ -1,77 +1,49 @@
 
 <?php
     <h2>FizzBuzzBang</h2>
-    <form id="fizzBuzzForm">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" value="Conan" required><br><br>
 
-        <label for="startNumber">Starting Number:</label>
-        <input type="number" id="startNumber" name="startNumber" value="1" required><br><br>
-
-        <label for="endNumber">Ending Number:</label>
-        <input type="number" id="endNumber" name="endNumber" value="100" required><br><br>
-
-        <label for="fizzWord">Fizz Word:</label>
-        <input type="text" id="fizzWord" name="fizzWord" value="fizz" required><br><br>
-
-        <label for="fizzNumber">Fizz Number:</label>
-        <input type="number" id="fizzNumber" name="fizzNumber" value="3" required><br><br>
-
-        <label for="buzzWord">Buzz Word:</label>
-        <input type="text" id="buzzWord" name="buzzWord" value="buzz" required><br><br>
-
-        <label for="buzzNumber">Buzz Number:</label>
-        <input type="number" id="buzzNumber" name="buzzNumber" value="5" required><br><br>
-
-        <label for="bangWord">Bang Word:</label>
-        <input type="text" id="bangWord" name="bangWord" value="bang" required><br><br>
-
-        <label for="bangNumber">Bang Number:</label>
-        <input type="number" id="bangNumber" name="bangNumber" value="7" required><br><br>
-
-        <button type="submit">Generate FizzBuzzBang</button>
+    <form method="post">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" value="<?php echo isset($_POST['name']) ? htmlspecialchars($_POST['name']) : 'Ferdinand'; ?>"><br>
+        <label for="start">Starting Number:</label><br>
+        <input type="number" id="start" name="start" value="<?php echo isset($_POST['start']) ? htmlspecialchars($_POST['start']) : ''; ?>"><br>
+        <label for="end">Ending Number:</label><br>
+        <input type="number" id="end" name="end" value="<?php echo isset($_POST['end']) ? htmlspecialchars($_POST['end']) : ''; ?>"><br>
+        <label for="fizzWord">Fizz Word:</label><br>
+        <input type="text" id="fizzWord" name="fizzWord" value="<?php echo isset($_POST['fizzWord']) ? htmlspecialchars($_POST['fizzWord']) : 'fizz'; ?>"><br>
+        <label for="fizzNum">Fizz Number:</label><br>
+        <input type="number" id="fizzNum" name="fizzNum" value="<?php echo isset($_POST['fizzNum']) ? htmlspecialchars($_POST['fizzNum']) : '3'; ?>"><br>
+        <label for="buzzWord">Buzz Word:</label><br>
+        <input type="text" id="buzzWord" name="buzzWord" value="<?php echo isset($_POST['buzzWord']) ? htmlspecialchars($_POST['buzzWord']) : 'buzz'; ?>"><br>
+        <label for="buzzNum">Buzz Number:</label><br>
+        <input type="number" id="buzzNum" name="buzzNum" value="<?php echo isset($_POST['buzzNum']) ? htmlspecialchars($_POST['buzzNum']) : '5'; ?>"><br>
+        <label for="bangWord">Bang Word:</label><br>
+        <input type="text" id="bangWord" name="bangWord" value="<?php echo isset($_POST['bangWord']) ? htmlspecialchars($_POST['bangWord']) : 'bang'; ?>"><br>
+        <label for="bangNum">Bang Number:</label><br>
+        <input type="number" id="bangNum" name="bangNum" value="<?php echo isset($_POST['bangNum']) ? htmlspecialchars($_POST['bangNum']) : '7'; ?>"><br><br>
+        <input type="submit" name="submit" value="Generate FizzBuzzBang">
     </form>
 
-    <div id="results"></div>
+    <?php
+    if(isset($_POST['submit'])){
+        $name = isset($_POST['name']) ? $_POST['name'] : 'Ferdinand';
+        $start = isset($_POST['start']) ? intval($_POST['start']) : 1;
+        $end = isset($_POST['end']) ? intval($_POST['end']) : 100;
+        $fizzWord = isset($_POST['fizzWord']) ? $_POST['fizzWord'] : 'fizz';
+        $fizzNum = isset($_POST['fizzNum']) ? intval($_POST['fizzNum']) : 3;
+        $buzzWord = isset($_POST['buzzWord']) ? $_POST['buzzWord'] : 'buzz';
+        $buzzNum = isset($_POST['buzzNum']) ? intval($_POST['buzzNum']) : 5;
+        $bangWord = isset($_POST['bangWord']) ? $_POST['bangWord'] : 'bang';
+        $bangNum = isset($_POST['bangNum']) ? intval($_POST['bangNum']) : 7;
 
-    <script>
-        document.getElementById("fizzBuzzForm").addEventListener("submit", function(event) {
-            event.preventDefault(); // Prevent default form submission
-            // Get form values
-            var name = document.getElementById("name").value;
-            var startNumber = parseInt(document.getElementById("startNumber").value);
-            var endNumber = parseInt(document.getElementById("endNumber").value);
-            var fizzWord = document.getElementById("fizzWord").value;
-            var fizzNumber = parseInt(document.getElementById("fizzNumber").value);
-            var buzzWord = document.getElementById("buzzWord").value;
-            var buzzNumber = parseInt(document.getElementById("buzzNumber").value);
-            var bangWord = document.getElementById("bangWord").value;
-            var bangNumber = parseInt(document.getElementById("bangNumber").value);
-
-            // Clear previous results
-            document.getElementById("results").innerHTML = "";
-
-            // Generate FizzBuzzBang
-            for (var i = startNumber; i <= endNumber; i++) {
-                var result = "";
-                if (i % fizzNumber === 0) result += fizzWord;
-                if (i % buzzNumber === 0) result += buzzWord;
-                if (i % bangNumber === 0) result += bangWord;
-                if (result === "") result = i;
-                document.getElementById("results").innerHTML += result + "<br>";
-            }
-        });
-
-        // Pre-populate form with default values
-        document.getElementById("fizzWord").value = "fizz";
-        document.getElementById("fizzNumber").value = "3";
-        document.getElementById("buzzWord").value = "buzz";
-        document.getElementById("buzzNumber").value = "5";
-        document.getElementById("bangWord").value = "bang";
-        document.getElementById("bangNumber").value = "7";
-    </script>
-	
-	
-		
- 
-             
+        echo "<h2>Hello, $name!</h2>";
+        echo "<p>Here is your FizzBuzzBang:</p>";
+        for ($i = $start; $i <= $end; $i++) {
+            $output = '';
+            if ($i % $fizzNum == 0) $output .= $fizzWord;
+            if ($i % $buzzNum == 0) $output .= $buzzWord;
+            if ($i % $bangNum == 0) $output .= $bangWord;
+            echo $output ? "<p>$output</p>" : "<p>$i</p>";
+        }
+    }
+    ?>
